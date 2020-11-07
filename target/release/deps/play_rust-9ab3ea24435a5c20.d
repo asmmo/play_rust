62de1bfb18487b4be73a84ab7ecfa9e279ec00a0
asmmo <ammarsayed99ph@gmail.com>
2020-11-07 19:08:32 +0200

    ignore

--- target/release/deps/play_rust-9ab3ea24435a5c20.d
+++
@@ -1,5 +0,0 @@
-/mnt/01D606E0BAE1CED0/code/play rust/target/release/deps/play_rust-9ab3ea24435a5c20: src/main.rs
-
-/mnt/01D606E0BAE1CED0/code/play rust/target/release/deps/play_rust-9ab3ea24435a5c20.d: src/main.rs
-
-src/main.rs:
--- target/release/play_rust.d
+++
@@ -1,1 +0,0 @@
-/mnt/01D606E0BAE1CED0/code/play\ rust/target/release/play_rust: /mnt/01D606E0BAE1CED0/code/play\ rust/src/main.rs
