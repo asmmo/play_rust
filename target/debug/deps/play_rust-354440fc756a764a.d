800da04a2c3afb163e124efd6b92eac75e36c8f4
asmmo <ammarsayed99ph@gmail.com>
2020-11-07 19:10:11 +0200

    ignore target

--- target/debug/deps/play_rust-354440fc756a764a.d
+++
@@ -1,5 +0,0 @@
-/mnt/01D606E0BAE1CED0/code/play rust/target/debug/deps/play_rust-354440fc756a764a: src/main.rs
-
-/mnt/01D606E0BAE1CED0/code/play rust/target/debug/deps/play_rust-354440fc756a764a.d: src/main.rs
-
-src/main.rs:
